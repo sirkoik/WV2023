6ad08721766b5516ed81e301b776c01ceba5c401
Alexander Koik-Cestone <akoikcestone@gmail.com>
2023-04-08 18:50:43 -0400

    Added candle, AWS badge stick, lights

--- resources/scripts/scene.js
+++ resources/scripts/scene.js
@@ -1,8 +1,11 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
 
 export { run };
+
+let newEnvMap;
 
 // setup scene, camera, and renderer.
 const scene = new THREE.Scene();
@@ -33,10 +36,12 @@ controls.autoRotateSpeed = 0.5;
 // candleFlames
 let candleFlame = {};
 let candleFlame2 = {};
+let candleFlame3 = {};
 
 let cube = {};
 
 let certBadge = null;
+let flag = null;
 
 // light
 const candleColor = 0xe7e06d;
@@ -45,7 +50,7 @@ light.intensity = 0.1;
 scene.add(light);
 
 // helper
-var axesHelper = new THREE.AxesHelper(10);
+const axesHelper = new THREE.AxesHelper(10);
 //scene.add( axesHelper );
 
 function run() {
@@ -65,6 +70,7 @@ function loadObjects() {
     function (gltf) {
       scene.add(gltf.scene);
       lightScene();
+      loadEnvMap();
       animate();
     },
     function (xhr) {
@@ -80,6 +86,7 @@ function loadObjects() {
 function lightScene() {
   candleFlame = scene.getObjectByName("CandleFlame1", true);
   candleFlame2 = scene.getObjectByName("CandleFlame2", true);
+  candleFlame3 = scene.getObjectByName("CandleFlame3", true);
 
   const material = new THREE.MeshStandardMaterial();
   material.emissive = new THREE.Color(0xe7e06d);
@@ -89,6 +96,7 @@ function lightScene() {
 
   candleFlame.material = material;
   candleFlame2.material = material;
+  candleFlame3.material = material;
 
   const light = new THREE.PointLight(candleColor, 0.5, 100);
   light.castShadow = true;
@@ -100,11 +108,26 @@ function lightScene() {
   light2.shadow.radius = 10;
   light2.position.set(0, 3, 0);
 
+  const light3 = new THREE.PointLight(candleColor, 0.5, 100);
+  light3.castShadow = true;
+  light3.shadow.radius = 10;
+  light3.position.set(0, 3, 0);
+
+  const certLight = new THREE.PointLight(candleColor, 1, 5);
+
+  const certLight2 = new THREE.PointLight(candleColor, 1, 5);
+  // light3.castShadow = true;
+  // light3.shadow.radius = 10;
+  certLight.position.set(0, 0, 0.2);
+  certLight2.position.set(0, 0, -0.2);
+
   //light2.add(cube);
 
   candleFlame.add(light);
 
   candleFlame2.add(light2);
+
+  candleFlame3.add(light3);
 
   const cakeBody = scene.getObjectByName("SimplifiedWV001", true);
   cakeBody.receiveShadow = true;
@@ -123,7 +146,7 @@ function lightScene() {
   const outline1 = scene.getObjectByName("ThreeOutline002");
   outline1.material = redGlowMaterial;
 
-  const flag = scene.getObjectByName("CNMI-Flag");
+  flag = scene.getObjectByName("CNMI-Flag");
   // flag.receiveShadow = true;
   flag.castShadow = true;
   // flag.material = new THREE.MeshLambertMaterial({
@@ -135,6 +158,17 @@ function lightScene() {
   flagpole.castShadow = true;
 
   certBadge = scene.getObjectByName("aws-da");
+  const certBadgeMesh = scene.getObjectByName("aws-da-mesh");
+
+  certBadge.castShadow = true;
+
+  certBadge.add(certLight);
+  certBadge.add(certLight2);
+
+  // certLight2.add(new THREE.AxesHelper(10));
+
+  // certBadgeMesh.material.emissive = new THREE.Color(0x0000ff);
+  // certBadgeMesh.material.emissiveIntensity = 0.2;
 
   [
     "ThreeOutline002",
@@ -146,6 +180,34 @@ function lightScene() {
   });
 
   console.log("outline1", outline1);
+}
+
+function loadEnvMap() {
+  const pmremGenerator = new THREE.PMREMGenerator(renderer);
+  pmremGenerator.compileEquirectangularShader();
+
+  new RGBELoader()
+    // .setDataType(THREE.UnsignedByteType)
+    .load("lenong_2_4k.hdr", (hdrEquiRect, textureData) => {
+      const hdrCubeRenderTarget =
+        pmremGenerator.fromEquirectangular(hdrEquiRect);
+      pmremGenerator.compileCubemapShader();
+      scene.background = hdrCubeRenderTarget.texture;
+      newEnvMap = hdrCubeRenderTarget.texture;
+
+      // apply envmap to flag
+      flag.material.envMap = newEnvMap;
+      flag.material.needsUpdate = true;
+      flag.material.envMapIntensity = 0.2;
+
+      // apply envmap to certBadge
+      certBadge.material.envMap = newEnvMap;
+      certBadge.material.needsUpdate = true;
+      certBadge.material.envMapIntensity = 0.2;
+
+      renderer.toneMapping = THREE.LinearToneMapping;
+      renderer.toneMappingExposure = 2;
+    });
 }
 
 const clock = new THREE.Clock();
@@ -164,9 +226,12 @@ function animate() {
   candleFlame2.rotation.x = -mult * Math.sin(5 + elapsed * 2);
   candleFlame2.rotation.y = -mult * Math.sin(5 + elapsed * 2);
 
-  certBadge.rotation.y = elapsed;
+  candleFlame3.rotation.x = -mult * Math.sin(5 + elapsed * 2);
+  candleFlame3.rotation.y = -mult * Math.sin(5 + elapsed * 2);
+
+  /*  certBadge.rotation.y = elapsed;
   certBadge.rotation.x = -mult * Math.sin(5 + elapsed * 2);
-  certBadge.rotation.z = -mult * Math.sin(5 + elapsed * 2);
+  certBadge.rotation.z = -mult * Math.sin(5 + elapsed * 2);*/
 
   renderer.render(scene, camera);
   controls.update();
